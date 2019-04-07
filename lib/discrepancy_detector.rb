c128d77405c81d221d77ae81a488f3698ef304ef
sandeep <spatlemail@gmail.com>
2019-04-08 01:25:18 +0200

    Add required files

---
+++ lib/discrepancy_detector.rb
@@ -0,0 +1,2 @@
+require './lib/discrepancy_detector/campaign_discrepancy_detector'
+require './lib/discrepancy_detector/app'
--- spec/lib/discrepancy_detector/app_spec.rb
+++ spec/lib/discrepancy_detector/app_spec.rb
@@ -3,9 +3,25 @@ require './lib/discrepancy_detector'
 
 describe DiscrepancyDetector::App do
 
-    subject { DiscrepancyDetector::App.new.run }
+  describe '.run' do
+    # mock the campaign API response
+    before(:each) do
+      stub_request(:get, 'https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df').with(
+        headers: {
+          'Accept'=>'*/*',
+          'User-Agent'=>'Ruby'
+        }
+      ).to_return(
+        status: 200,
+        body: File.read('./spec/campaigns_response_with_discrepancy.json')
+      )
+    end
 
-    context 'when pass valid action name' do
+    subject { DiscrepancyDetector::App.new.run(argv) }
+
+    context 'when pass valid action request' do
+      let(:argv) { ['campaign'] }
+
       it 'should not raise error' do
          expect { subject }.not_to raise_error
       end
@@ -15,7 +31,9 @@ describe DiscrepancyDetector::App do
       end
     end
 
-    context 'when pass invalid action name' do
+    context 'when pass invalid action request' do
+      let(:argv) { [] }
+
       it 'should raise error' do
          expect { subject }.to raise_error('We do not support this action, please enter a valid action.')
       end
