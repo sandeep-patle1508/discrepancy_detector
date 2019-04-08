769d1e91b5baabd9984fad635ca6e1dfc26ec315
sandeep <spatlemail@gmail.com>
2019-04-08 02:24:53 +0200

    WIP - Add the discrepancy finding logic in Campaign Discrepancy Detector service

--- lib/discrepancy_detector/campaign_discrepancy_detector.rb
+++ lib/discrepancy_detector/campaign_discrepancy_detector.rb
@@ -1,7 +1,35 @@
+require './lib/models/campaign'
 require './lib/discrepancy_detector/callable'
 
 module DiscrepancyDetector
   class CampaignDiscrepancyDetector
     include Callable
+
+    DISCREPANCY_ATTRIBUTES = { status: 'status', ad_description: 'description'}.freeze
+
+    def initialize
+      @local_campaigns = Campaign.all
+      @remote_campaigns = Campaign.get_remote_campaigns
+      @missing_remote_campaigns = []
+      @changed_campaigns = []
+    end
+
+    def call
+      result = {}
+
+      @local_campaigns.each do |local_campaign|
+        discrepancies = []
+        remote_campaign = remote_campaign_by_local_reference(local_campaign.external_reference)
+
+      end
+
+      result
+    end
+
+    private
+
+    def remote_campaign_by_local_reference(campaign_reference)
+      @remote_campaigns.find { |remote_campaign| remote_campaign['reference'] == campaign_reference }
+    end
   end
 end
